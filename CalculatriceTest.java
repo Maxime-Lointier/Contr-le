5fbb18fe9e63dd20aabb21241e2bd680993b5920
LOINTIER Maxime <12300457@q10428.iutv.univ-paris13.fr>
2025-05-23 10:31:13 +0200

    premier test

--- CalculatriceTest.java
+++ CalculatriceTest.java
@@ -1,3 +1,19 @@
-public class CalculatriceTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+public class CalculatriceTest {
+    private Calculatrice calculatrice;
+    @BeforeEach
+    void SetUp(){
+        calculatrice = new Calculatrice();
+    }
+
+
+    @Test
+    public void TestAddition(){
+      float resultat = calculatrice.add(2,3);
+      assertEquals(5,resultat);
+    }
 }
